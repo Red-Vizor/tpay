4047d7aecce682502d50553e68c7f85be23aea45
Ömer Emre Elmalı <omeremreelma@gmail.com>
2021-10-29 13:53:55 +0300

    Update

--- assets/script/swiper-js/swiper.js
+++ assets/script/swiper-js/swiper.js
@@ -1,16 +1,68 @@
-const swiper = new Swiper('.swiper', {
-    direction: 'vertical',
+const swiperPartner = new Swiper('.swiper-partner', {
     loop: true,
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
+    slidesPerView: 1,
+    spaceBetween: 10,
+    breakpoints: {
+        320: {
+            slidesPerView: 2,
+            spaceBetween: 20
+        },
+        480: {
+            slidesPerView: 3,
+            spaceBetween: 30
+        },
+        640: {
+            slidesPerView: 4,
+            spaceBetween: 40
+        }
+    }
+});
+
+const swiperVision = new Swiper('.swiper-vision', {
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 10,
+    breakpoints: {
+        320: {
+            slidesPerView: 1.2,
+            spaceBetween: 20
+        },
+        480: {
+            slidesPerView: 3,
+            spaceBetween: 30
+        },
+        640: {
+            slidesPerView: 4,
+            spaceBetween: 40
+        },
+        992: {
+            slidesPerView: 4,
+            spaceBetween: 40
+        }
+    }
+});
+
+
+const swiperFeatures = new Swiper('.swiper-features', {
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 10,
+    breakpoints: {
+        320: {
+            slidesPerView: 1.2,
+            spaceBetween: 10
+        },
+        480: {
+            slidesPerView: 3,
+            spaceBetween: 10
+        },
+        640: {
+            slidesPerView: 4,
+            spaceBetween: 10
+        },
+        992: {
+            slidesPerView: 4,
+            spaceBetween: 10
+        }
+    }
+});
